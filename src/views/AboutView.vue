19080999fc04b5b5f3902b8097dadce0ca1767e5
NikusM <nicolas.marquillier@gmail.com>
2024-02-29 17:26:18 +0100

    init projet - try Pinia - ENDED

---
+++ src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { useCounterStore } from '@/stores/counter'
+
+const counter = useCounterStore()
+</script>
+
+<template>
+  <div>
+    <div class="about">
+      <div>
+        <h1>This is an about page</h1>
+      </div>
+      <p>Count: {{ counter.count }}</p>
+      <p>Double Count: {{ counter.doubleCount }}</p>
+    </div>
+    <div>
+      <button @click="counter.increment()">increment</button>
+      <button @click="counter.decrement()">decrement</button>
+    </div>
+  </div>
+</template>
+
+<style></style>
